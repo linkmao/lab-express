7f5aa5fffca27a4cd448ad4a38ccd5ba51ac6c2c
maolink <maolink@yahoo.com>
2024-11-19 17:50:22 -0500

    first branch commit

--- src/config/bd.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_URL)
-.then(db=>console.log("base de datos conectada con exito, info"))
-.catch(err=>console.log(err))
--- src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-const faviconError = (req, res, next) => {
-  if (req.originalUrl === '/favicon.ico') {
-      return res.status(204).end() // Responder sin contenido
-  }
-  next()
-}
-
-module.exports = faviconError
