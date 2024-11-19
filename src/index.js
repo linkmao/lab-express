9a773e34f918b42d575e64c76548a61609c378ac
maolink <maolink@yahoo.com>
2024-11-19 18:09:25 -0500

    19.11.2024 servidor basico ok

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app= express()
+
+app.set('port',3000)
+app.listen(app.get('port'),()=>{console.log("Servidor escuchando en el puerto "+ app.get('port') )})
